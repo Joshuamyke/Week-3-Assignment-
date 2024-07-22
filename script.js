feb848ec4c2b7535678ded958dea7848e28eafa3
Mba Joshua Chinonyerem <94995354+Joshuamyke@users.noreply.github.com>
2024-07-22 17:52:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var myName = "Mba Joshua Chinonyerem" // String
+var myAge = 25                     // Number
+var single = true;                //Boolean
+var crazyBoy = null;              // Null
+var = undefined;                 // Underfined
+var car = {Name: 'Volve', color: 'black'}      // Object
+var collection = [1, 2, 3, 4, 5];
